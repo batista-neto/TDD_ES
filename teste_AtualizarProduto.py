da37943318f1fec857cd4d2367b0e0665304c0a7
João Batista <jbneto9@gmail.com>
2023-06-12 12:28:56 -0300

    implementado teste de Atualizar Produto

---
+++ teste_AtualizarProduto.py
@@ -0,0 +1,30 @@
+import unittest
+from Produto import Produto
+from Sistema import Sistema
+
+class TestDeletarProduto(unittest.TestCase):
+
+    def test_atualizar_produto(self):
+        produto1 = Produto(1, 'arroz', 19.90)
+        sistema = Sistema()
+        sistema.adicionar_produto(produto1)
+        resposta = sistema.atualizar_produto(1, "feijao", 19.90)
+        self.assertEqual(resposta, "Produto atualizado com sucesso")
+
+    def test_atualizar_produto_nao_existe(self):
+        produto1 = Produto(1, 'arroz', 19.90)
+        sistema = Sistema()
+        sistema.adicionar_produto(produto1)
+        resposta = sistema.atualizar_produto(2, "feijao", 19.90)
+        self.assertEqual(resposta, "Produto com esse ID não encontrado, tente novamente")
+
+    def test_atualizar_produto_sem_nome(self):
+        produto1 = Produto(1, 'arroz', 19.90)
+        sistema = Sistema()
+        sistema.adicionar_produto(produto1)
+        resposta = sistema.atualizar_produto(2, None, 19.90)
+        self.assertEqual(resposta, "Nome não foi inserido, tente novamente")
+
+
+if __name__ == '__main__':
+    unittest.main()
