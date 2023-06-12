7264036b53d111e62494cb1a1bce9addcc7b635c
Emanuel Valentim <ervalentim@gmail.com>
2023-06-12 01:18:55 -0300

    Refatorando AdicionarProduto.py acrescentando método para listar produtos

--- AdicionarProduto.py
+++ AdicionarProduto.py
@@ -1,3 +1,5 @@
+from Produto import Produto
+
 class AdicionarProduto:
     def __init__(self):
         self.produtos_adicionados = []
@@ -6,9 +8,25 @@ class AdicionarProduto:
         if produto.id is None:
             return "Produto sem id, tente novamente"
 
-        for produto in self.produtos_adicionados:
-            if produto.id == produto.id:
+        for prod in self.produtos_adicionados:
+            if prod.id == produto.id:
                 return "Produto com esse ID já existe, tente novamente"
 
         self.produtos_adicionados.append(produto)
-        return "Adicionado com sucesso"
+        print("Adicionado com sucesso")
+        return self.produtos_adicionados
+
+    def listar_produtos(self):
+        return [produto.__dict__ for produto in self.produtos_adicionados]
+
+#adicionar_produto = AdicionarProduto()    
+
+# a = Produto(1, 'arroz', 19.90)
+# produto2 = Produto(2, 'Produto B', 20.99)
+
+# adicionar_produto.adicionar_produto(a)
+# adicionar_produto.adicionar_produto(produto2)
+
+# c = adicionar_produto.listar_produtos()
+# d = c
+# print(len(c[1]))
